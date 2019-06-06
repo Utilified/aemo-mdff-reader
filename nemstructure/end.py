107fbd03a857140340e285c271862979c0c01cb8
cohen.benergy <cohen.r@benergy.net.au>
2019-06-06 11:41:59 +1000

    Completed Reader class

--- nemstructure/end.py
+++ nemstructure/end.py
@@ -10,5 +10,6 @@ class EndRecord(Record):
     Represents an end record in
     the NEM-12 file.
     """
+    RECORD_ID = RECORD_ID
     def __init__(self):
         super().__init__(RECORD_ID, ATTRIBUTES)
--- nemstructure/nem12.py
+++ nemstructure/nem12.py
@@ -2,6 +2,7 @@
 # import statements
 from nemstructure.record import Record, MANDATORY, REQUIRED, NOTREQUIRED
 ATTRIBUTES = {}
+MINUTES_DAILY = 24 * 60
 
 NMI_RECORD_ID = 200
 ATTRIBUTES[NMI_RECORD_ID] = [
@@ -18,7 +19,7 @@ ATTRIBUTES[NMI_RECORD_ID] = [
 ]
 
 INTERVAL_RECORD_ID = 300
-INTERVAL_LENGTHS = [30, 15]
+INTERVAL_LENGTHS = ["30", "15"]
 ATTRIBUTES[INTERVAL_RECORD_ID] = [
     ("RecordIndicator", MANDATORY),
     ("IntervalDate", MANDATORY),
@@ -54,6 +55,7 @@ class NMIData(Record):
     Represenents a NMI Data details record
     in the NEM-12 file.
     """
+    RECORD_ID = NMI_RECORD_ID
     def __init__(self):
         super().__init__(NMI_RECORD_ID, ATTRIBUTES[NMI_RECORD_ID])
 
@@ -62,14 +64,20 @@ class IntervalData(Record):
     Represents an Interval data record
     in the NEM-12 file
     """
-    def __init__(self):
-        super().__init__(INTERVAL_RECORD_ID, ATTRIBUTES[INTERVAL_RECORD_ID])
+    RECORD_ID = INTERVAL_RECORD_ID
+    def __init__(self, interval_type):
+        intervals = [("IntervalValue%d" % i, MANDATORY)
+                     for i in range(1, MINUTES_DAILY // int(interval_type) + 1)]
+        attributes = ATTRIBUTES[INTERVAL_RECORD_ID][:2] + \
+            intervals + ATTRIBUTES[INTERVAL_RECORD_ID][3:]
+        super().__init__(INTERVAL_RECORD_ID, attributes)
 
 class IntervalEvent(Record):
     """
     Represents an Interval event record
     in the NEM-12 file
     """
+    RECORD_ID = EVENT_RECORD_ID
     def __init__(self):
         super().__init__(EVENT_RECORD_ID, ATTRIBUTES[EVENT_RECORD_ID])
 
@@ -78,5 +86,6 @@ class B2BDetails(Record):
     Represents an B2B details record
     in the NEM-12 file
     """
+    RECORD_ID = B2B_RECORD_ID
     def __init__(self):
         super().__init__(B2B_RECORD_ID, ATTRIBUTES[B2B_RECORD_ID])
