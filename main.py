b2026749259faf32dc88a625259148cf27ac390c
Cohen Robinson <cohenrobinson@utilified.com>
2022-10-04 22:07:19 +1100

    minor updates

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import configparser
 import os
 from glob import glob
 
-CONFIG_DIR = 'bin//config.cfg'
+CONFIG_DIR = 'bin/config.cfg'
 SECTION = 'TEST'
 
 def main():
@@ -16,7 +16,7 @@ def main():
     config.read(CONFIG_DIR)
     # read all files and store in memory
     files_read = [file
-                 for path, subdir, files in os.walk(config[SECTION]['Directory'])
+                 for path, _, _ in os.walk(config[SECTION]['Directory'])
                  for file in glob(os.path.join(path, '*.csv'))]
     # now begin iterating through read files and store in DB
     storer = Storer(dict(config[SECTION]))
