debe3f3359db90993a617aedd8fc2b19359029ba
Cohen <cohenrobinson@icloud.com>
2023-02-13 22:35:49 +1100

    restructured files

--- nem12_reader/nemstructure/nem13.py
+++ nem12_reader/nemstructure/nem13.py
@@ -1,6 +1,6 @@
 "NMI data details record for nem-reader-imported"
 # import statements
-from nemstructure.record import Record, MANDATORY, REQUIRED, NOTREQUIRED
+from .record import Record, MANDATORY, REQUIRED, NOTREQUIRED
 ATTRIBUTES = {}
 
 ACCUMULATION_RECORD_ID = 250
