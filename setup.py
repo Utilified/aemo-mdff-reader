3376e710e43355f913b65714799ab1801dee4de1
Cohen <cohenrobinson@icloud.com>
2023-02-13 22:46:55 +1100

    amended setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    author='Utilified Holdings Pty Ltd',
+    description='',
+    name='nem12-reader',
+    version='0.1.0',
+    packages=find_packages(include=[
+        'nem12_reader',
+        'nem12_reader.*'
+    ]),
+    python_requires='>=3.8'
+)
